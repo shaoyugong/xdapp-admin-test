b4bfd1c5742954ed639f413f3b1284831d884eb9
龚少瑜 <gongshaoyu@xindong.com>
2018-12-14 11:14:07 +0800

    修改路由，基础页面布局

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import App from './views/App'
 import Element from './config/element'
 import i18n from './config/i18n'
 import store from './config/store'
