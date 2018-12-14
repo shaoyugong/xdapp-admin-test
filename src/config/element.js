7a67dbebdd128c1c8f82e4611739aedb10fe3c7f
龚少瑜 <gongshaoyu@xindong.com>
2018-12-14 15:15:50 +0800

    修改路由，基础页面布局3

--- src/config/element.js
+++ src/config/element.js
@@ -1,6 +1,6 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Element from 'element-ui'
-import '../assets/theme/element-variables.scss'
+import '../assets/theme/element-def.scss'
 import '../assets/css/index.scss'
 
 export default Element
